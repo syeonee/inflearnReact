1cc67858196e43b3e257afe3079a37e3b490945f
syeonee <syeoneee326@gmail.com>
2023-02-27 15:30:50 +0900

    study: 간단한 일기장 프로젝트 - React에서 사용자 입력 처리하기 강의 실습 내용

---
+++ diary/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DiaryEdiotr from './DiaryEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <DiaryEdiotr />
+    </div>
+  );
+}
+
+export default App;
