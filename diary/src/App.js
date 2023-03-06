77ba0aad73f122fd85a575c58bd635024af29fae
syeonee <syeoneee326@gmail.com>
2023-03-06 17:05:30 +0900

    react에서 배열 사용하기1 - DiaryItem 컴포넌트 생성

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,11 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import DiaryEdiotr from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: "syeon",
+    content: "Hi~~~",
+    emotion: 3,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 2,
+    author: "syeon",
+    content: "Lucy~",
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 3,
+    author: "syeon",
+    content: "~~~~~",
+    emotion: 2,
+    created_date: new Date().getTime()
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEdiotr />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
