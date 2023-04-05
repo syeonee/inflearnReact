728d09398e3aae7f301305a56757afb436af4553
syeonee <syeoneee326@gmail.com>
2023-04-05 23:59:38 +0900

    study: 수정하기 버튼 까지의 실습

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -1,48 +1,128 @@
-import './App.css';
+import React, { useReducer, useRef } from "react";
+
+import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home';
-import New from './pages/New';
+import Home from "./pages/Home";
+import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
-// Components
-import MyButton from "./components/MyButton"
-import MyHeader from './components/MyHeader';
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== it.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) => it.id === action.data.id ? { ...action.data } : it);
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+const dummyData = [
+  {
+    id: 1,
+    emotion: 1,
+    content: "today's diary 1",
+    date: 1638969241918,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "today's diary 2",
+    date: 1638969241919,
+  },
+  {
+    id: 3,
+    emotion: 3,
+    content: "today's diary 3",
+    date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    emotion: 4,
+    content: "today's diary 4",
+    date: 1638969241921,
+  },
+  {
+    id: 5,
+    emotion: 5,
+    content: "today's diary 5",
+    date: 1638969241922,
+  },
+  {
+    id: 6,
+    emotion: 5,
+    content: "today's diary 6",
+    date: new Date().getTime() + 1,
+  },
+];
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, dummyData);
+
+  const dataId = useRef(0);
+  //CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    });
+    dataId.current += 1;
+  };
+  //REMOVE
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
+  //EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion
+      }
+    });
+  };
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader
-          headText={"App"}
-          leftChild={
-            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
-          }
-          rightChild={
-            <MyButton text={"오른쪽 버튼"} onClick={() => alert("오른쪽 클릭")} />
-          }
-        />
-        <h2>App.js</h2>
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"positive"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"negative"}
-        />
-        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/diary/:id" element={<Diary />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{onCreate, onEdit, onRemove}} >
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
